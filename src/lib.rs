0c5b380bd20a2cb1ddc6149e58eb71e8f7663bcf
Marcin S <scatman@bu.edu>
2018-05-09 21:55:37 +0700

    [MAID-2652] chore/license: update to dual license (MIT/BSD)

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 // Copyright 2018 MaidSafe.net limited.
 //
-// This SAFE Network Software is licensed to you under The General Public License (GPL), version 3.
-// Unless required by applicable law or agreed to in writing, the SAFE Network Software distributed
-// under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied. Please review the Licences for the specific language governing
-// permissions and limitations relating to use of the SAFE Network Software.
+// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// http://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
+// https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
+// modified, or distributed except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use of the SAFE Network
+// Software.
 
 //! get_if_addrs
+
 #![doc(html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/
 maidsafe_logo.png",
       html_favicon_url = "http://maidsafe.net/img/favicon.ico",
@@ -14,18 +16,18 @@ maidsafe_logo.png",
 // For explanation of lint checks, run `rustc -W help` or see
 // https://github.com/maidsafe/QA/blob/master/Documentation/Rust%20Lint%20Checks.md
 #![forbid(exceeding_bitshifts, mutable_transmutes, no_mangle_const_items, unknown_crate_types,
-         warnings)]
+          warnings)]
 #![deny(bad_style, deprecated, improper_ctypes, missing_docs, non_shorthand_field_patterns,
-       overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
-       stable_features, unconditional_recursion, unknown_lints, unsafe_code, unused,
-       unused_allocation, unused_attributes, unused_comparisons, unused_features, unused_parens,
-       while_true)]
+        overflowing_literals, plugin_as_library, private_no_mangle_fns, private_no_mangle_statics,
+        stable_features, unconditional_recursion, unknown_lints, unsafe_code, unused,
+        unused_allocation, unused_attributes, unused_comparisons, unused_features, unused_parens,
+        while_true)]
 #![warn(trivial_casts, trivial_numeric_casts, unused_extern_crates, unused_import_braces,
-       unused_qualifications, unused_results)]
+        unused_qualifications, unused_results)]
 #![allow(box_pointers, missing_copy_implementations, missing_debug_implementations,
          variant_size_differences)]
 #![cfg_attr(feature = "cargo-clippy",
-           deny(clippy, unicode_not_nfc, wrong_pub_self_convention, option_unwrap_used))]
+            deny(clippy, unicode_not_nfc, wrong_pub_self_convention, option_unwrap_used))]
 #![cfg_attr(feature = "cargo-clippy", allow(use_debug, too_many_arguments))]
 
 #[cfg(windows)]
